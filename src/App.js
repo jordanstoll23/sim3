cac9c1072dcbc18258a911756ac01576855b0d65
Andrew-Cunningham <andrewjasoncunningham@yahoo.com>
2017-09-22 10:10:27 -0700

    updated css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import router from './router';
+import logo from './assets/logo.png';
 
 class App extends Component {
   render() {
@@ -11,7 +12,7 @@ class App extends Component {
     <div>
       <div className="App">
         <div className='splashScreen'>
-          <div className='logo'></div>
+        <img src={logo} alt={logo} className="Auth__logo_img"/>
           <div className="helo">Helo</div>
           <div><a className='text' href="http://localhost:3535/auth"><div className="loginButton"><div>Login / Register</div></div></a></div>
         </div>
